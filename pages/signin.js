23e04b1bff4d43e17bcc70a4a600de8f04fa51dc
vicktor349 <ayomidevictor349@gmail.com>
2022-12-28 17:18:03 +0100

    Made Toggling Form

--- pages/signin.js
+++ pages/signin.js
@@ -1,15 +1,13 @@
-import { useToggle, upperFirst } from '@mantine/hooks';
-import { useForm } from '@mantine/form';
+import react, { useState } from 'react';
 import {
     TextInput,
     PasswordInput,
-    Text,
-    Paper,
-    Group,
     Button,
-    Divider,
+    Text,
+    Group,
     Anchor,
-    Stack,
+    Divider,
+    Paper
 } from '@mantine/core';
 import Head from 'next/head';
 import { FcGoogle } from 'react-icons/fc'
@@ -17,82 +15,98 @@ import { GrFacebook } from 'react-icons/gr'
 import styles from '../styles/signin.module.css'
 
 const signin = () => {
-    const [type, toggle] = useToggle(['signin', 'signup']);
-    const form = useForm({
-        initialValues: {
-            email: '',
-            name: '',
-            password: '',
-        }
-    });
+    const [authMode, setAuthMode] = useState('signup')
+    const changeAuthMode = () => {
+        setAuthMode(authMode === "signup" ? "signin" : "signup")
+    }
+    if (authMode === 'signin') {
+        return (
+            <div>
+                <Paper radius="md" p="xl" withBorder className={styles.paper}>
+                    <Text size="lg" weight={500}>
+                        Register An Account
+                    </Text>
+                    <TextInput
+                        label="Email"
+                        type='email'
+                        placeholder="Please Enter Your Email Address"
+                        style={{ margin: '0.7rem 0 0.7rem 0' }}
+                    />
+                    <TextInput
+                        label="First Name"
+                        type='text'
+                        placeholder="Please Enter Your First Name"
+                        style={{ margin: '0.7rem 0 0.7rem 0' }}
+                    />
+                    <TextInput
+                        label="Last Name"
+                        type='text'
+                        placeholder="Please Enter Your Last Name"
+                        style={{ margin: '0.7rem 0 0.7rem 0' }}
+                    />
+                    <PasswordInput
+                        label="Password"
+                        placeholder="Please Enter Your Password"
+                        style={{ margin: '0.7rem 0 0.7rem 0' }}
 
-    return (
-        <>
-            <Head>
-                <title>
-                    Favourite Gadget | {type}
-                </title>
-            </Head>
-            <Paper radius="md" p="xl" withBorder className={styles.paper}>
-                <Text size="lg" weight={500}>
-                    Welcome, {type}
-                </Text>
-
-
-                <form onSubmit={form.onSubmit(() => { })}>
-                    <Stack>
-                        {type === 'signup' && (
-                            <div>
-                                <TextInput
-                                    label="First Name"
-                                    placeholder="Please Enter Your First Name"
-                                    style={{ margin: '0.7rem 0 0.7rem 0' }}
-                                />
-                                <TextInput
-                                    label="Name"
-                                    placeholder="Please Enter Your Last Name"
-                                />
-                            </div>
-
-                        )}
-
-                        <TextInput
-                            required
-                            label="Email"
-                            placeholder="Please Enter Your Email Address"
-                            style={{ margin: '0.7rem 0 0.7rem 0' }}
-                        />
-
-                        <PasswordInput
-                            required
-                            label="Password"
-                            placeholder="Please Enter Your Password"
-                        />
-                    </Stack>
-
+                    />
+                    <PasswordInput
+                        label="ReType Password"
+                        placeholder="Please Retype Your Password"
+                        style={{ margin: '0.7rem 0 0.7rem 0' }}
+                    />
                     <Group position="apart" mt="xl">
                         <Anchor
                             component="button"
                             type="button"
                             color="dimmed"
-                            onClick={() => toggle()}
+                            onClick={changeAuthMode}
                             size="xs"
                         >
-                            {type === 'signup'
-                                ? 'Already have an account? Login'
-                                : "Don't have an account? Register"}
+                            Already have an account? Login
                         </Anchor>
-                        <Button type="submit">{upperFirst(type)}</Button>
+                        <Button type="submit">Sign Up</Button>
                     </Group>
-                </form>
-                <Divider label="Or" labelPosition="center" my="lg" />
+                    <Divider label="Or" labelPosition="center" my="lg" />
 
-                <Group grow mb="md" mt="md">
-                    <div className={styles.google} style={{ border: '1px solid #dee2e6', borderRadius: '6px', padding: '0.625rem' }}><FcGoogle radius="xl" className={styles.FcGoogle} style={{ marginRight: '10px' }} />Google</div>
-                    <div className={styles.facebook} style={{ border: '1px solid #dee2e6', borderRadius: '6px', padding: '0.625rem' }}><GrFacebook radius="xl" className={styles.GrFacebook} style={{ marginRight: '10px' }} />Facebook</div>
+                    <Group grow mb="md" mt="md">
+                        <div className={styles.google} style={{ border: '1px solid #dee2e6', borderRadius: '6px', padding: '0.625rem' }}><FcGoogle radius="xl" className={styles.FcGoogle} style={{ marginRight: '10px' }} />Google</div>
+                        <div className={styles.facebook} style={{ border: '1px solid #dee2e6', borderRadius: '6px', padding: '0.625rem' }}><GrFacebook radius="xl" className={styles.GrFacebook} style={{ marginRight: '10px' }} />Facebook</div>
+                    </Group>
+                </Paper>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <Paper radius="md" p="xl" withBorder className={styles.paper}>
+                <Text size="lg" weight={500}>
+                    Login To Your Account
+                </Text>
+                <TextInput
+                    label="Email"
+                    type='email'
+                    placeholder="Please Enter Your Email Address"
+                    style={{ margin: '0.7rem 0 0.7rem 0' }}
+                />
+                <TextInput
+                    label="Password"
+                    placeholder="Please Enter Your Password"
+                />
+                <Group position="apart" mt="xl">
+                    <Anchor
+                        component="button"
+                        type="button"
+                        color="dimmed"
+                        onClick={changeAuthMode}
+                        size="xs"
+                    >
+                        Don't have an account? Register
+                    </Anchor>
+                    <Button type="submit">Sign In</Button>
                 </Group>
             </Paper>
-        </>
-    );
+        </div>
+    )
 }
 export default signin
